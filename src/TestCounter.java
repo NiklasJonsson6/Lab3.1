fa073f7d12745c84dbe7c152cb2dcf3a9ce854ee
Niklas <bknjniklas@gmail.com>
2015-10-03 17:45:35 +0200

    TestCounter changes

--- src/TestCounter.java
+++ src/TestCounter.java
@@ -1,15 +1,29 @@
-import org.w3c.dom.css.Counter;
-
 /**
  * Created by Niklas on 2015-09-21.
  */
 public class TestCounter {
     public static void main(String[] args){
-        boolean error = false;
 
         CounterModel c1 = new CounterModel(10); //create test counters
         CounterModel c2 = new CounterModel(10);
 
+        if (!testMethods(c1, c2)){
+            System.out.println("No errors! \n");
+        }
+
+        CounterModel[] ca = new CounterModel[14];
+        testArray(ca, 12);
+    }
+
+    /**
+     * Tests the CounterModel methods
+     * @param c1 First CounterModel to test
+     * @param c2 Seconds CounterModel to test
+     * @return True if error(s) is found
+     */
+    static boolean testMethods(CounterModel c1, CounterModel c2) {
+
+        boolean error = false;
         if (!(c1.getValue() == 0 || c2.getValue() == 0)){
             System.out.println("Wrong starting value, should be 0!");
             error = true;
@@ -47,19 +61,24 @@ public class TestCounter {
         }
 
         //For manual toString check
-        System.out.println(c1 + "\n" + c2);
+        System.out.println("c1: " + c1 + "\n" + "c2: " + c2);
 
-        if (!error){
-            System.out.println("No errors! \n");
-        }
+        return error;
+    }
 
-        CounterModel[] ca = new CounterModel[10];
+    /**
+     * Tests an array of CounterModels
+     * @param ca The CounterModel array
+     * @param mod The modulus value of the counters
+     */
+    static void testArray(CounterModel[] ca, int mod) {
+
         for(int i = 0; i < ca.length; i++) {
-            ca[i] = new CounterModel(10);
+            ca[i] = new CounterModel(mod);
         }
-        System.out.println("Should have same value as counter number:");
+        System.out.println("Should have same value as counter number % " + mod);
         int i = 0;
-        for (int k = 0; k < 10; k++) {
+        for (int k = 0; k < ca.length; k++) {
             while (i < k) {
                 ca[k].increment();
                 i++;
@@ -71,5 +90,6 @@ public class TestCounter {
 
     void changeValue(CounterModel r) {
         //TODO
+        //Vad ska metoden göra...?
     }
 }
